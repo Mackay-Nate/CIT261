804a181ff6d671c52fb3b63d93a92362b29ce75c
Nate <mac17039@byui.edu>
2019-10-08 21:07:11 -0600

    added clipboard and mouse events

--- Events/events.js
+++ Events/events.js
@@ -1,8 +1,14 @@
 
 
 function moreRoom(list) { 
-  document.getElementById(list).style.lineHeight = "35px";
- /* document.getElementById(list).style.padding = "10px"; */
+  document.getElementById(list).style.lineHeight = "40px";
 }
 
+function lessRoom(list) { 
+  document.getElementById(list).style.lineHeight = "19px";  
+}
+
+function showMe(item) { 
+  document.getElementById(item).style.display = "initial";
+}
 
