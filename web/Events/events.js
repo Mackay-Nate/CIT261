e8f3bc2c42357c5cf0766cefc1aebb6353bf3840
Nate <mac17039@byui.edu>
2019-10-09 23:04:27 -0600

    initial javaScript Events commit

---
+++ web/Events/events.js
@@ -0,0 +1,14 @@
+
+
+function moreRoom(list) { 
+  document.getElementById(list).style.lineHeight = "40px";
+}
+
+function lessRoom(list) { 
+  document.getElementById(list).style.lineHeight = "19px";  
+}
+
+function showMe(item) { 
+  document.getElementById(item).style.display = "initial";
+}
+
