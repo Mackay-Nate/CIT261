d19c108b154ddf240abeafaee7b4940110c4a281
Nate <mac17039@byui.edu>
2019-10-10 22:55:23 -0600

    added Transition, Animation; hide/show

--- web/Events/events.js
+++ web/Events/events.js
@@ -5,10 +5,32 @@ function moreRoom(list) {
 }
 
 function lessRoom(list) { 
-  document.getElementById(list).style.lineHeight = "19px";  
+  document.getElementById(list).style.lineHeight = "19px";
+
+  var hide = document.getElementsByClassName('hideMe');
+  for (var i = 0; i < hide.length; i++) { 
+    hide[i].style.display = "none";
+  }
+  document.getElementById(list).style.background = "white";
 }
 
 function showMe(item) { 
-  document.getElementById(item).style.display = "initial";
+  if (document.getElementById(item).style.display != "initial") {
+    document.getElementById(item).style.display = "initial";
+  } else {
+    document.getElementById(item).style.display = "none";
+    document.getElementById(item).style.background = "white";
+  }
+}
+
+
+function mouse(item) { 
+  if (item.style.background != 'lightblue') { 
+    item.style.background = 'lightblue'; 
+    item.style.fontSize = '200%';
+  } else { 
+    item.style.fontSize = '100%';
+    item.style.background = 'white'
+  }
 }
 
